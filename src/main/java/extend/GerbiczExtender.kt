99301e43c2b0846e026071e124bc73865863f2ec
Volker Leck <volker.leck@gmail.com>
2018-05-20 19:28:48 +0200

    wrap squareCache access

--- src/main/java/extend/GerbiczExtender.kt
+++ src/main/java/extend/GerbiczExtender.kt
@@ -5,6 +5,8 @@ import util.squareCache
 // see http://www.mersenneforum.org/showpost.php?s=b2d1b7da337e837bfe622060c950c7a5&p=477406&postcount=9
 object GerbiczExtender {
 
+    private inline fun isSquare(number: Int) = squareCache.contains(number)
+
     private fun indexPairs() = generateSequence(Pair(0, 1)) {
         if (it.first + 1 == it.second)
             Pair(0, it.second + 1)
@@ -13,7 +15,7 @@ object GerbiczExtender {
     }
 
     private fun squaresIn(range: IntRange): List<Int> {
-        return range.filter(squareCache::contains)
+        return range.filter(::isSquare)
     }
 
     private fun squareExtenders(number: Int): Sequence<Pair<Int, Int>> {
@@ -29,7 +31,7 @@ object GerbiczExtender {
         return squareExtenders(n)
                 .map { (s1, s2) -> (s1 - n) to (s2 - n) }
                 .filter { (v1, v2) ->
-                    squareCache.contains(cycle.next(v1) + cycle.next(v2))
+                    isSquare(cycle.next(v1) + cycle.next(v2))
                 }
     }
 
